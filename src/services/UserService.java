d04bfc666c941b4ddcd5a1bae513cf4f2cd329ff
SchaellerThibault <schaeller.thibault@gmail.com>
2018-03-08 11:07:51 +0100

    Accept ContactRequest OK

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -7,6 +7,7 @@ package services;
 
 import entity.ContactRequest;
 import entity.User;
+import interfaces.ContactRequestInterface;
 import interfaces.UserInterface;
 import interfaces.UserServiceInterface;
 import java.rmi.RemoteException;
@@ -19,6 +20,7 @@ import repository.UserRepository;
  * @author thibault
  */
 public class UserService implements UserServiceInterface {
+
     private UserRepository userRepository;
 
     /**
@@ -27,59 +29,87 @@ public class UserService implements UserServiceInterface {
     public UserService() {
         this.userRepository = new UserRepository();
     }
-    
-    
+
+    /**
+     *
+     * @param userName
+     * @return
+     * @throws java.lang.Exception
+     */
+    @Override
+    public UserInterface getUser(String userName) throws RemoteException, Exception {
+        UserInterface user = this.userRepository.getUser(userName);
+        if (user == null) {
+            throw new Exception("[USER SERVICE] User " + userName + " not found");
+        }
+
+        return user;
+    }
+
     /**
      * Authenticate a user from his username and the plainPassword
-     * 
+     *
      * @param username
      * @param plainPassword
      * @return boolean
-     * @throws RemoteException 
+     * @throws RemoteException
      */
     @Override
     public boolean authenticateUser(String username, String plainPassword) throws RemoteException {
         UserInterface user = this.userRepository.getUser(username);
-        
+
         return user != null && user.getPassword().equals(utils.Security.encodePassword(plainPassword));
     }
 
     /**
      * Send a contactRequest from senderUserName to receiverUserName
+     *
      * @param senderUserName
      * @param receiverUserName
      * @return
      * @throws RemoteException
-     * @throws Exception 
+     * @throws Exception
      */
     @Override
     public boolean sendContactRequest(String senderUserName, String receiverUserName) throws RemoteException, Exception {
         User sender = new User(this.userRepository.getUser(senderUserName));
         User receiver = new User(this.userRepository.getUser(receiverUserName));
-        
-        if (sender == null)
+
+        if (sender == null) {
             throw new Exception("[USER SERVICE] " + senderUserName + " not found");
-        
-        if (receiver == null)
+        }
+
+        if (receiver == null) {
             throw new Exception("[USER SERVICE] " + receiverUserName + " not found");
-        
+        }
+
         receiver.addContactRequest(new ContactRequest(sender, receiver, new Date()));
-        
+
         return true;
     }
 
     /**
-     * 
-     * @param userName
-     * @return 
-     * @throws java.lang.Exception 
+     *
+     * @param answer
+     * @param request
+     * @throws RemoteException
      */
     @Override
-    public UserInterface getUser(String userName) throws RemoteException, Exception{
-        UserInterface user = this.userRepository.getUser(userName);
-        if (user == null)
-            throw new Exception("[USER SERVICE] User " + userName + " not found");
+    public UserInterface answerToContactRequest(boolean answer, ContactRequestInterface request) throws RemoteException {
+        if (answer) {
+            System.out.println("[USER SERVICE] Accept contactRequest : " + request.getReceiver().getUsername());
+            request.setStatus(ContactRequestInterface.CONTACT_REQUEST_STATUS_ACCEPTED);
+
+            request.getSender().addContact(request.getReceiver());
+            request.getReceiver().addContact(request.getSender());
+        } else {
+            System.out.println("[USER SERVICE] Refuse contactRequest : " + request.getReceiver().getUsername());
+
+            request.setStatus(ContactRequestInterface.CONTACT_REQUEST_STATUS_REFUSED);
+        }
+
+        request.getReceiver().removeContactRequest(request);
         
-        return user;
+        return request.getReceiver();
     }
 }
--- src/utils/Security.java
+++ src/utils/Security.java
@@ -15,23 +15,23 @@ import java.util.logging.Logger;
  * @author thibault
  */
 public class Security {
-    
-   private static MessageDigest md;
 
-   public static String encodePassword(String plainPassword){
-    try {
-        md = MessageDigest.getInstance("MD5");
-        byte[] passBytes = plainPassword.getBytes();
-        md.reset();
-        byte[] digested = md.digest(passBytes);
-        StringBuffer sb = new StringBuffer();
-        for(int i=0;i<digested.length;i++){
-            sb.append(Integer.toHexString(0xff & digested[i]));
+    private static MessageDigest md;
+
+    public static String encodePassword(String plainPassword) {
+        try {
+            md = MessageDigest.getInstance("MD5");
+            byte[] passBytes = plainPassword.getBytes();
+            md.reset();
+            byte[] digested = md.digest(passBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < digested.length; i++) {
+                sb.append(Integer.toHexString(0xff & digested[i]));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Security.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return sb.toString();
-    } catch (NoSuchAlgorithmException ex) {
-        Logger.getLogger(Security.class.getName()).log(Level.SEVERE, null, ex);
-    }
         return null;
-   }
+    }
 }
